eacd75e08b4323f19e0473c2d01d47b797aee0a8
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-02-22 21:55:54 -0300

    Added axios dependencie

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+// import api from "./services/api";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          onClick={() => {
-            console.log("opaaa");
-          }}
-        />
+        <button onClick={() => {}} />
       </header>
     </div>
   );
